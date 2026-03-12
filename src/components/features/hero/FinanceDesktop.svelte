82b4cf8fa1c457958f93cc86152c1d58ae5c0bfa
María García <mara.garcia.garcia.j@gmail.com>
2026-03-12 15:12:43 +0100

    Footer layout update: move ENISA between logo and links, keep contact CTA layout, adjust navbar logo

--- src/components/features/hero/FinanceDesktop.svelte
+++ src/components/features/hero/FinanceDesktop.svelte
@@ -1,25 +1,8 @@
 <script>
 	import Container from "@components/Container.svelte";
-	import FlowCarousel from "@components/FlowCarousel.svelte";
-	import BracketedContent from "@components/BracketedContent.svelte";
 </script>
 
 <Container
 	className="xl:flex hidden flex-col items-center justify-center pb-24"
 	bgColor="bg-primary-panther">
-	<div class="w-full">
-		<div class="flex flex-col items-center justify-center gap-6">
-			<h4
-				class="text-primary-beige/60 text-base font-light text-center 2xl:text-xl">
-				Financed by
-			</h4>
-			<a href="https://www.enisa.es/" target="_blank" rel="noopener noreferrer" class="cursor-pointer">
-				<img
-					width={120}
-					src="/assets/enisa-logo.png"
-					alt="Enisa Logo"
-					class="object-contain mx-7 2xl:mx-9" />
-			</a>
-		</div>
-	</div>
 </Container>
